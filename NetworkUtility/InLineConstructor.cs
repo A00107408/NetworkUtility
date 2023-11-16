a7bb2d4d441702744fd89e1fcbe8c13361da7359
eogha <eogha@ARMADILLO>
2023-11-16 13:23:30 +0000

    Add project files.

---
+++ NetworkUtility/InLineConstructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility
+{
+    internal class InLineConstructor(int a , int b)
+    {
+        private int ThisInta = a;
+        private int ThisIntb = b;
+
+        public int AddConstructorParams()
+        {
+            return ThisInta + ThisIntb;
+        }
+    }
+}
---
+++ NetworkUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetworkUtility;
+
+Console.WriteLine("Hello, World!");
+
+static void Main(string[] args)
+{
+    InLineConstructor ILC = new InLineConstructor(1, 2);
+
+    var result = ILC.AddConstructorParams();
+    Console.WriteLine("Result: {0}", result);
+}
