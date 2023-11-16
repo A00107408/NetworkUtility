ee41c4882a0900b8d3d8868ca0592f1f54923e2e
Eoghan Hynes <eoghan.hynes@gmail.com>
2023-11-16 14:02:32 +0000

    My first commit message from within VS 17.8.0.

--- NetworkUtility/Program.cs
+++ NetworkUtility/Program.cs
@@ -3,10 +3,4 @@ using NetworkUtility;
 
 Console.WriteLine("Hello, World!");
 
-static void Main(string[] args)
-{
-    InLineConstructor ILC = new InLineConstructor(1, 2);
-
-    var result = ILC.AddConstructorParams();
-    Console.WriteLine("Result: {0}", result);
-}
+Console.WriteLine("Cahlo bro");
